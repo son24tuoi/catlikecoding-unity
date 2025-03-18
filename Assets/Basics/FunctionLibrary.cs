2aed566bae0e8e4bd9c32e4eda06696ea4efac67
Sonnh <Sonnh@apusgame.vn>
2025-03-18 17:46:41 +0700

    Basics

---
+++ Assets/Basics/FunctionLibrary.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using static UnityEngine.Mathf;
+
+public static class FunctionLibrary
+{
+    public static float Wave(float x, float t)
+    {
+        return Sin(PI * (x + t));
+    }
+    
+    public static float MultiWave(float x, float t)
+    {
+        float y = Sin(PI * (x + t));
+        y += Sin(2f * PI * (x + t)) * 0.5f;
+        return y * (2f / 3f);
+    }
+}
---
+++ Assets/Basics/Graph.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    [SerializeField] private Transform pointPrefab;
+    [SerializeField][Range(10, 100)] private int resolution = 10;
+
+    private Transform[] points;
+
+    private void Awake()
+    {
+        float step = 2f / resolution;
+        Vector3 position = Vector3.zero;
+        Vector3 scale = Vector3.one * step;
+        points = new Transform[resolution];
+        for (int i = 0; i < resolution; i++)
+        {
+            Transform point = Instantiate(pointPrefab);
+            position.x = (i + 0.5f) * step - 1f;
+            position.y = FunctionLibrary.MultiWave(position.x, 0f);
+            point.localPosition = position;
+            point.localScale = scale;
+            point.SetParent(transform, false);
+            points[i] = point;
+        }
+    }
+
+    private void Update()
+    {
+        float time = Time.time;
+        for (int i = 0; i < points.Length; i++)
+        {
+            Vector3 position = points[i].localPosition;
+            position.y = FunctionLibrary.MultiWave(position.x, time);
+            points[i].localPosition = position;
+        }
+    }
+}
