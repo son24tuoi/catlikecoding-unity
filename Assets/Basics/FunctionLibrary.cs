4d1c87ceb5b4055b7c4ec6428f9915a37f9fa738
Sonnh <Sonnh@apusgame.vn>
2025-03-28 16:51:52 +0700

    + Add more functions

--- Assets/Basics/FunctionLibrary.cs
+++ Assets/Basics/FunctionLibrary.cs
@@ -6,21 +6,73 @@ using static UnityEngine.Mathf;
 
 public static class FunctionLibrary
 {
-    public static float Wave(float x, float t)
+    public delegate Vector3 Function(float u, float v, float t);
+
+    public enum FunctionName { Wave, MultiWave, Ripple, Sphere, Torus }
+
+    private static Function[] functions = {
+        Wave,
+        MultiWave,
+        Ripple,
+        Sphere,
+        Torus,
+    };
+
+    public static Function GetFunction(FunctionName name)
     {
-        return Sin(PI * (x + t));
+        return functions[(int)name];
     }
 
-    public static float MultiWave(float x, float t)
+    public static Vector3 Wave(float u, float v, float t)
     {
-        float y = Sin(PI * (x + t));
-        y += Sin(2f * PI * (x + t)) * 0.5f;
-        return y * (2f / 3f);
+        Vector3 p;
+        p.x = u;
+        p.y = Sin(PI * (u + v + t));
+        p.z = v;
+        return p;
     }
 
-    public static float Ripple(float x, float t)
+    public static Vector3 MultiWave(float u, float v, float t)
     {
-        float d = Abs(x);
-        return d;
+        Vector3 p;
+        p.x = u;
+        p.y = Sin(PI * (u + t));
+        p.y += Sin(2f * PI * (v + t)) * 0.5f;
+        p.y *= 1f / 2.5f;
+        p.z = v;
+        return p;
+    }
+
+    public static Vector3 Ripple(float u, float v, float t)
+    {
+        float d = Sqrt(u * u + v * v);
+        Vector3 p;
+        p.x = u;
+        p.y = Sin(PI * (4f * d - t));
+        p.y /= 1f + 10f * d;
+        p.z = v;
+        return p;
+    }
+
+    public static Vector3 Sphere(float u, float v, float t)
+    {
+        float r = Cos(0.5f * PI * v);
+        Vector3 p;
+        p.x = r * Sin(PI * u * t);
+        p.y = Sin(PI * 0.5f * v);
+        p.z = r * Cos(PI * u * t);
+        return p;
+    }
+
+    public static Vector3 Torus(float u, float v, float t)
+    {
+        float r1 = 0.75f;
+        float r2 = 0.25f;
+        float s = r1 + r2 * Cos(PI * v);
+        Vector3 p;
+        p.x = s * Sin(PI * u * t);
+        p.y = r2 * Sin(PI * v);
+        p.z = s * Cos(PI * u * t);
+        return p;
     }
 }
--- Assets/Basics/Graph.cs
+++ Assets/Basics/Graph.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,46 +7,41 @@ public class Graph : MonoBehaviour
 {
     [SerializeField] private Transform pointPrefab;
     [SerializeField][Range(10, 100)] private int resolution = 10;
-    [SerializeField][Range(0, 2)] private int function = 0;
+    [SerializeField] private FunctionLibrary.FunctionName function;
 
     private Transform[] points;
+    private float time;
+    private FunctionLibrary.Function f;
+    private float step;
 
     private void Awake()
     {
-        float step = 2f / resolution;
-        Vector3 position = Vector3.zero;
+        step = 2f / resolution;
         Vector3 scale = Vector3.one * step;
-        points = new Transform[resolution];
-        for (int i = 0; i < resolution; i++)
+        points = new Transform[resolution * resolution];
+        for (int i = 0; i < points.Length; i++)
         {
-            Transform point = Instantiate(pointPrefab);
-            position.x = (i + 0.5f) * step - 1f;
-            position.y = function switch
-            {
-                0 => FunctionLibrary.Wave(position.x, 0f),
-                1 => FunctionLibrary.MultiWave(position.x, 0f),
-                _ => FunctionLibrary.Ripple(position.x, 0f)
-            };
-            point.localPosition = position;
+            Transform point = points[i] = Instantiate(pointPrefab);
             point.localScale = scale;
             point.SetParent(transform, false);
-            points[i] = point;
         }
     }
 
     private void Update()
     {
-        float time = Time.time;
-        for (int i = 0; i < points.Length; i++)
+        time = Time.time;
+        f = FunctionLibrary.GetFunction(function);
+        float v = 0.5f * step - 1f;
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
         {
-            Vector3 position = points[i].localPosition;
-            position.y = function switch
+            if (x == resolution)
             {
-                0 => FunctionLibrary.Wave(position.x, time),
-                1 => FunctionLibrary.MultiWave(position.x, time),
-                _ => FunctionLibrary.Ripple(position.x, time)
-            };
-            points[i].localPosition = position;
+                x = 0;
+                z += 1;
+                v = (z + 0.5f) * step - 1f;
+            }
+            float u = (x + 0.5f) * step - 1f;
+            points[i].localPosition = f(u, v, time);
         }
     }
 }
